59e7f64af1adb23b2e176898911fd430d4e056c6
Trong Hieu DalatCoder <hieuntctk42@gmail.com>
2020-09-22 07:46:01 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Implement CORS | Allow everyone for consume API
+app.use(cors());
+
+// Implement CORS for complex request (PUT PATCH DELETE)
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('/api/directory', require('./routes/directory'));
+app.use('/api/file', require('./routes/file'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
