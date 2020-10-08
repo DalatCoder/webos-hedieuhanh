d1e25377b583015541e5f4a0893b58b63b831de2
Trong Hieu DalatCoder <hieuntctk42@gmail.com>
2020-10-08 11:50:46 +0700

    shorten string more than 40 chars

--- client/src/app/utils/shortenString.js
+++ client/src/app/utils/shortenString.js
@@ -1,5 +1,5 @@
 export default function shortenString(rawString) {
   if (rawString.length < 30) return rawString;
-  const chars = rawString.split('').slice(0, 30).join('');
+  const chars = rawString.split('').slice(0, 40).join('');
   return `${chars} ...`;
 }
