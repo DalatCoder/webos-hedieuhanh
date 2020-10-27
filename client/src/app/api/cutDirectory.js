83c925cbe87c608fafaf3413c337f5de2ccc384b
leaimy <hantctk42@gmail.com>
2020-10-27 15:27:29 +0700

    Hoan thanh chuc nang cut

---
+++ client/src/app/api/cutDirectory.js
@@ -0,0 +1,26 @@
+const cutDirectory = (src, dest, name) => {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify({ src, dest, name });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  return new Promise((resolve) => {
+    fetch('http://localhost:4000/api/directory/cut', requestOptions)
+      .then((response) => response.json())
+      .then((result) => resolve(result))
+      .catch((err) => {
+        console.error(err);
+        alert('Oops! Something went wrong!');
+        resolve(null);
+      });
+  });
+};
+
+export default cutDirectory;
